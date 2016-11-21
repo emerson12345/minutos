c1f5596841921dc74773c2bb3751c1fdc8c31710
Emerson Arando <emereaq@hotmail.com>
2016-11-21 09:32:35 -0400

    instituciones

--- app/Models/Institucion.php
+++ app/Models/Institucion.php
@@ -3,6 +3,7 @@
 namespace Sicere\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 /**
  * Class Institucion
@@ -22,10 +23,13 @@ class Institucion extends Model
         'inst_telf2',
         'inst_fax',
         'inst_email',
-        'inst_nit',
+        'inst_responsable',
         'dep_id',
         'prov_id',
         'mun_id',
+        'area_id',
+        'inst_urbano_rural',
+        'inst_nro_camas',
         'inst_localidad',
         'inst_direccion_zona',
         'inst_direccion_calle',
@@ -50,5 +54,18 @@ class Institucion extends Model
 
     public function municipio(){
         return $this->belongsTo('Sicere\Models\LugarMunicipio','mun_id','mun_id');
+    }
+    
+    public static function get_institucion()
+    {
+        $instituciones = DB::table('institucion' )
+            ->select('inst_codigo','inst_nombre','inst_telf1','inst_direccion_calle','dep_nombre', 'prov_nombre','mun_nombre','area_nombre')
+            ->join('lugar_departamento', 'lugar_departamento.dep_id', '=', 'institucion.dep_id')
+            ->join('lugar_provincia', 'lugar_provincia.prov_id', '=', 'institucion.prov_id')
+            ->join('lugar_municipio', 'lugar_municipio.mun_id', '=', 'institucion.mun_id')
+            ->join('lugar_area', 'lugar_area.area_id', '=', 'institucion.area_id')
+            ->orderby('lugar_departamento.dep_id','lugar_municipio.mun_id')
+            ->get();
+        return $instituciones;
     }
 }
