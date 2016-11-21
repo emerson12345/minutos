4274daf663fdafae0950c8f91e406223805dee25
Emerson Arando <emereaq@hotmail.com>
2016-11-21 12:13:19 -0400

    CODIGO DE SEGURIDAD

--- app/Http/Controllers/ReporteController.php
+++ app/Http/Controllers/ReporteController.php
@@ -3,7 +3,7 @@
 namespace Sicere\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Auth;
 use Sicere\Http\Requests;
 use Sicere\Http\Controllers\Controller;
 use PDF;
@@ -19,15 +19,17 @@ class ReporteController extends Controller
     public function produccionPDF(Request $request){
         PDF::setHeaderCallback(function($pdf) {
             $pdf->Cell(0, 27, '', 'B', false, 'R', 0, '', 0, false, 'T', 'M');
-            $pdf->Image(asset('template/dist/img/bolivia.gif'), 25, 10, 0, 15, 'GIF', 'http://www.tcpdf.org', '', true, 150, '', false, false, 0, false, false, false);
+            $pdf->Image(asset('template/dist/img/bolivia.gif'), 25, 10, 0, 15, 'GIF', 'https://www.minsalud.gob.bo', '', true, 150, '', false, false, 0, false, false, false);
             $pdf->SetFont('helvetica', 'B', 18);
-            $pdf->Text(60,14,'Ministerio de salud y deportes');
-            $pdf->Image(asset('template/dist/img/minsalud-logo.jpg'), 25, 12, 0, 12, 'JPG', 'http://www.tcpdf.org', '', true, 150, 'R', false, false, 0, false, false, false);
+            $pdf->Text(60,14,'Sistema de centros de rehabilitación');
+            $pdf->Image(asset('template/dist/img/minsalud-logo.jpg'), 25, 12, 0, 12, 'JPG', 'https://www.minsalud.gob.bo', '', true, 150, 'R', false, false, 0, false, false, false);
         });
         PDF::setFooterCallback(function($pdf) {
+            $strCodSeguridad=session('institucion')->inst_codigo . '|' . session('institucion')->inst_nombre .'|' . Auth::user()->user_id;
             $pdf->SetY(-15);
             $pdf->SetFont('helvetica', 'I', 8);
             $pdf->Cell(0, 10, 'Pagina '.$pdf->getAliasNumPage().'/'.$pdf->getAliasNbPages(), 'T', false, 'R', 0, '', 0, false, 'T', 'M');
+            $pdf->write2DBarcode($strCodSeguridad, 'PDF417', 25, 196, 150, 6, null, 'N',true);
         });
 
 
