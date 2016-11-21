52d92959ce14a846c9553149a71165b640625369
percypedro <percyfuentesramos@gmail.com>
2016-11-21 15:22:36 -0400

    Agenda

--- resources/views/PacienteHc/agenda.blade.php
+++ resources/views/PacienteHc/agenda.blade.php
@@ -16,15 +16,12 @@
 @section('breadcrumb')
     <ol class="breadcrumb">
         <li>
-
-        </li>
-        <li>
-
+            <a href="{{route('adm.paciente.index')}}">Roles</a>
         </li>
     </ol>
-    @stop
+@stop
 
-    @section('content')
+@section('content')
         <style>
             #calendar {
                 max-width: 900px;
@@ -43,85 +40,84 @@
     @stop
 
 @section('script')
-<link rel='stylesheet' href="{{asset('template/plugins/fullcalendar/fullcalendar.css')}}"/>
-<link rel='stylesheet' media='print' href="{{asset('template/plugins/fullcalendar/fullcalendar.print.css')}}" />
-<script src="{{asset('template/plugins/fullcalendar/lib/moment.min.js')}}"></script>
-<script src="{{asset('template/plugins/fullcalendar/lib/jquery.min.js')}}"></script>
-<script src="{{asset('template/plugins/fullcalendar/fullcalendar.min.js')}}"></script>
-<script src="{{asset('template/plugins/fullcalendar/locale/es.js')}}"></script>
-<script>
+    <link rel='stylesheet' href="{{asset('template/plugins/fullcalendar/fullcalendar.css')}}"/>
+    <link rel='stylesheet' media='print' href="{{asset('template/plugins/fullcalendar/fullcalendar.print.css')}}" />
+    <script src="{{asset('template/plugins/fullcalendar/lib/moment.min.js')}}"></script>
+    <script src="{{asset('template/plugins/fullcalendar/fullcalendar.min.js')}}"></script>
+    <script src="{{asset('template/plugins/fullcalendar/locale/es.js')}}"></script>
+    <script>
 
-    $(document).ready(function() {
+        $(document).ready(function() {
 
-        $('#calendar').fullCalendar({
-            header: {
-                left: 'prev,next today',
-                center: 'title',
-                right: 'month,agendaWeek,agendaDay,listWeek'
-            },
-            defaultDate: '2016-09-12',
-            navLinks: true, // can click day/week names to navigate views
-            editable: true,
-            eventLimit: true, // allow "more" link when too many events
-            events: [
-                {
-                    title: 'All Day Event',
-                    start: '2016-09-01'
+            $('#calendar').fullCalendar({
+                header: {
+                    left: 'prev,next today',
+                    center: 'title',
+                    right: 'month,agendaWeek,agendaDay,listWeek'
                 },
-                {
-                    title: 'Long Event',
-                    start: '2016-09-07',
-                    end: '2016-09-10'
-                },
-                {
-                    id: 999,
-                    title: 'Repeating Event',
-                    start: '2016-09-09T16:00:00'
-                },
-                {
-                    id: 999,
-                    title: 'Repeating Event',
-                    start: '2016-09-16T16:00:00'
-                },
-                {
-                    title: 'Conference',
-                    start: '2016-09-11',
-                    end: '2016-09-13'
-                },
-                {
-                    title: 'Meeting',
-                    start: '2016-09-12T10:30:00',
-                    end: '2016-09-12T12:30:00'
-                },
-                {
-                    title: 'Lunch',
-                    start: '2016-09-12T12:00:00'
-                },
-                {
-                    title: 'Meeting',
-                    start: '2016-09-12T14:30:00'
-                },
-                {
-                    title: 'Happy Hour',
-                    start: '2016-09-12T17:30:00'
-                },
-                {
-                    title: 'Dinner',
-                    start: '2016-09-12T20:00:00'
-                },
-                {
-                    title: 'Birthday Party',
-                    start: '2016-09-13T07:00:00'
-                },
-                {
-                    title: 'Click for Google',
-                    url: 'http://google.com/',
-                    start: '2016-09-28'
-                }
-            ]
+                defaultDate: '2016-09-12',
+                navLinks: true, // can click day/week names to navigate views
+                editable: true,
+                eventLimit: true, // allow "more" link when too many events
+                events: [
+                    {
+                        title: 'All Day Event',
+                        start: '2016-09-01'
+                    },
+                    {
+                        title: 'Long Event',
+                        start: '2016-09-07',
+                        end: '2016-09-10'
+                    },
+                    {
+                        id: 999,
+                        title: 'Repeating Event',
+                        start: '2016-09-09T16:00:00'
+                    },
+                    {
+                        id: 999,
+                        title: 'Repeating Event',
+                        start: '2016-09-16T16:00:00'
+                    },
+                    {
+                        title: 'Conference',
+                        start: '2016-09-11',
+                        end: '2016-09-13'
+                    },
+                    {
+                        title: 'Meeting',
+                        start: '2016-09-12T10:30:00',
+                        end: '2016-09-12T12:30:00'
+                    },
+                    {
+                        title: 'Lunch',
+                        start: '2016-09-12T12:00:00'
+                    },
+                    {
+                        title: 'Meeting',
+                        start: '2016-09-12T14:30:00'
+                    },
+                    {
+                        title: 'Happy Hour',
+                        start: '2016-09-12T17:30:00'
+                    },
+                    {
+                        title: 'Dinner',
+                        start: '2016-09-12T20:00:00'
+                    },
+                    {
+                        title: 'Birthday Party',
+                        start: '2016-09-13T07:00:00'
+                    },
+                    {
+                        title: 'Click for Google',
+                        url: 'http://google.com/',
+                        start: '2016-09-28'
+                    }
+                ]
+            });
+
         });
 
-    });
-
-</script>
+    </script>
 @stop
