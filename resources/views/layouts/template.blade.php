16def33345bf0d0a9e8a1020ff044b04ce227639
percypedro <percyfuentesramos@gmail.com>
2016-11-21 15:59:16 -0400

    search

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -244,17 +244,6 @@
                     </a>
                 </div>
             </div>
-            <!-- search form -->
-            <form action="#" method="get" class="sidebar-form">
-                <div class="input-group">
-                    <input type="text" name="q" class="form-control" placeholder="Search...">
-              <span class="input-group-btn">
-                <button type="submit" name="search" id="search-btn" class="btn btn-flat"><i class="fa fa-search"></i>
-                </button>
-              </span>
-                </div>
-            </form>
-            <!-- /.search form -->
             <!-- sidebar menu: : style can be found in sidebar.less -->
             <?php $menu = \Sicere\Models\Aplicacion::where('app_renderiza',1)->where('app_nivel_menu',1)->get();?>
             <ul class="sidebar-menu">
