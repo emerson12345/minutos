b68118bc1bf8d60f17ad63cbb11360602b623a70
PACF <pedro.acf.3@gmail.com>
2016-12-11 17:17:07 -0400

    arreglos

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -7,6 +7,7 @@ use Illuminate\Validation\Rule;
 use Illuminate\Support\Facades\Auth;
 use Sicere\Models\Institucion;
 use Illuminate\Support\Facades\Hash;
+use Sicere\Models\ReportTemplate;
 use Sicere\Models\Rrhh;
 use Sicere\Models\Usuario;
 use Validator;
@@ -111,31 +112,9 @@ class UsuarioController extends Controller
     }
 
     public function report(){
-        PDF::setHeaderCallback(function($pdf) {
-            $pdf->Cell(0, 27, '', 'B', false, 'R', 0, '', 0, false, 'T', 'M');
-            $pdf->Image(asset('template/dist/img/bolivia.gif'), 15, 10, 0, 15, 'GIF', '', '', true, 150, '', false, false, 0, false, false, false);
-            $pdf->SetFont('helvetica', 'B', 11);
-            $pdf->Text(33,22,'Sistema de centros de rehabilitación','R');
-            $pdf->SetFont('helvetica', 'K', 10);
-            $pdf->Text(15,27,'Establecimiento: '.session('institucion')->inst_nombre);
-            $pdf->Image(asset('template/dist/img/minsalud-logo.jpg'), 25, 12, 0, 12, 'JPG', '', '', true, 150, 'R', false, false, 0, false, false, false);
-        });
-        PDF::setFooterCallback(function($pdf) {
-            $strCodSeguridad=session('institucion')->inst_codigo . '|' . session('institucion')->inst_nombre .'|' . Auth::user()->user_id;
-            $pdf->SetY(-15);
-            $pdf->SetFont('helvetica', 'I', 8);
-            $pdf->Cell(0, 10, 'Pagina '.$pdf->getAliasNumPage().'/'.$pdf->getAliasNbPages(), 'T', false, 'R', 0, '', 0, false, 'T', 'M');
-            //$pdf->write2DBarcode(bcrypt('Mi super codigo'), 'PDF417', 25, 275, 150, 6, null, 'N',true);
-            $pdf->write2DBarcode($strCodSeguridad, 'PDF417', 15, 283, 100, 6, null, 'N',true);
-            //$pdf->writeQR($strCodSeguridad, 'PDF417', 25, 275, 150, 6, null, 'N',true);
-        });
-        PDF::SetTitle('Usuarios');
-        PDF::SetSubject('Reporte de sistema');
-        PDF::SetMargins(15, 30, 15);
-        PDF::SetFontSubsetting(false);
-        PDF::SetFontSize('10px');
-        PDF::SetAutoPageBreak(TRUE, PDF_MARGIN_BOTTOM);
+        ReportTemplate::printHeaderFooter();
         PDF::AddPage('P', 'Letter');
+        ReportTemplate::printTitle('USUARIOS REGISTRADOS EN EL SISTEMA');
         PDF::writeHTML(view('usuario.report')->render(), true, false, true, false, '');
         PDF::lastPage();
         PDF::Output('usuario.pdf');
--- resources/views/usuario/report.blade.php
+++ resources/views/usuario/report.blade.php
@@ -1,6 +1,4 @@
 <?php
-PDF::writeHTML('<br><div align="center" style="font-family: Calibri; font-size: 11px; font-weight: bold;"><strong>USUARIOS REGISTRADOS EN EL SISTEMA</strong></div>');
-//PDF::Text(43,22,'Lista de usuarios');
 PDF::SetFillColor(217, 217, 217);
 PDF::SetTextColor(0);
 PDF::SetDrawColor(0, 0, 0);
@@ -11,7 +9,7 @@ $w = array(32, 32, 39,32, 32, 15);
 $header = ['Nombre de usuario','Nombre','Email','Fecha de alta','Fecha de ult. modf.','Vigente'];
 $num_headers = count($header);
 for($i = 0; $i < $num_headers; ++$i) {
-    PDF::Cell($w[$i], 7, $header[$i], 0, 0, 'C', 1);
+    PDF::Cell($w[$i], 7, $header[$i], 0, 0, 'C', 1,null,1);
 }
 PDF::Ln();
 
@@ -23,7 +21,7 @@ PDF::SetFont('');
 $fill = 0;
 //for($i=0;$i<10;$i++)
 foreach(\Sicere\User::all() as $user) {
-    PDF::Cell($w[0], 6, $user->user_codigo, 'R', 0, 'L', $fill,null,0);
+    PDF::Cell($w[0], 6, $user->user_codigo, 'R', 0, 'L', $fill,null,1);
     PDF::Cell($w[1], 6, $user->user_nombre, 'RL', 0, 'L', $fill,null,1);
     PDF::Cell($w[2], 6, $user->user_email, 'RL', 0, 'L', $fill,null,0);
     PDF::Cell($w[3], 6, $user->user_fec_alta->format('d/m/Y H:i'), 'RL', 0, 'L', $fill,null,1);
